f7e301e83b73bd28c5bbe132e7b192cfbc59acaa
Bartosz Macek <bartoszmacek@gmail.com>
2019-09-29 15:28:07 +0100

    Routing completed (navigation to each component from navbar working fine) Home Component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ServicesComponent} from './services/services.component';
+import {GalleryComponent} from './gallery/gallery.component';
+import {TestimonialsComponent} from './testimonials/testimonials.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ' ', component: AppComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'testimonials', component: TestimonialsComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { AboutUsComponent } from './about-us/about-us.component';
     NavbarComponent,
     HeaderComponent,
     FooterComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
